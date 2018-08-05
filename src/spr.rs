8b77a83f57cfcff209e7782ce944861b3106b554
Shawak <maxi.nussbaum@googlemail.com>
2018-08-06 00:21:21 +0200

    don't export images

--- src/spr.rs
+++ src/spr.rs
@@ -68,91 +68,10 @@ pub fn parse(filename: String) -> Result<SpriteData, Error> {
                     read += 4 + (3 /* channels */ * colored_count);
                 }
 
-                img.save(format!("test/{}.png", n.0))?;
+                //img.save(format!("test/{}.png", n.0))?;
 
                 Ok((n.0, img))
             })
-            //.collect::<HashMap<_, _>>(),
             .collect::<Result<_, Error>>()?,
     })
 }
-
-/*pub fn parse(filename: String) -> Result<SpriteData, Error> {
-    let mut file = File::open(filename)?;
-    let mut data: Vec<u8> = Vec::new();
-    file.read_to_end(&mut data)?;
-    let mut data = Cursor::new(data);
-
-    let mut spr = SpriteData {
-        version: data.get()?,
-        sprites: HashMap::new(),
-    };
-
-    // multi thread
-    let sprite_count = data.get::<u32>()?;
-    let mut lookup: Vec<(u32, u32)> = Vec::with_capacity(sprite_count as usize);
-    let arc = Arc::new(Mutex::new(spr));
-
-    for id in 1..sprite_count + 1 {
-        let addr = data.get::<u32>()?;
-        if addr == 0 {
-            continue;
-        }
-        lookup.push((id, addr));
-    }
-
-    (1..10).collect();
-
-    lookup.par_iter().for_each(|n| {
-        let mut img: Image = ImageBuffer::new(32, 32);
-
-        arc.lock().unwrap().sprites.insert(n.0, img);
-        //spr.sprites.insert(n.0, img);
-    });
-
-
-    // single thread
-    /*let sprite_count = data.get::<u32>()?;
-    for k in 1..sprite_count + 1 {
-        let addr = data.get::<u32>()?;
-        if addr == 0 {
-            continue;
-        }
-
-        let mut img: Image = ImageBuffer::new(32, 32);
-
-        let before = data.position();
-        data.seek(SeekFrom::Start(addr as u64))?;
-
-        let color_key = data.get::<[u8; 3]>()?;
-        let size = data.get()?;
-
-        let mut write = 0;
-        let mut read = 0;
-        let mut i = 0;
-        while read < size && write < SPRITE_DATA_SIZE {
-            let transparent_count = data.get::<u16>()?;
-            let colored_count = data.get::<u16>()?;
-
-            for _ in 0..transparent_count {
-                img.get_pixel_mut(i / 32, i % 32).data = [0, 0, 0, 0];
-                i += 1;
-            }
-
-            for _ in 0..colored_count {
-                let (r, g, b, a) = (data.get()?, data.get()?, data.get()?, 0);
-                img.get_pixel_mut(i / 32, i % 32).data = [r, g, b, a];
-                i += 1;
-            }
-
-            write += 4 * transparent_count as u32 + 4 * colored_count as u32;
-            read += 4 + (3 /* channels */ * colored_count);
-        }
-
-        // img.save(format!("test/{}.png", k))?;
-        spr.sprites.insert(k, img);
-        data.seek(SeekFrom::Start(before as u64))?;
-    }*/
-
-    Ok(arc.to_owned().to_owned().get_mut().)
-}*/
